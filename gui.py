d852ea3f89e3350feae5c71b882afa634b179e61
z7x8c9 <bublekvass228@gmail.com>
2025-02-08 22:02:24 +0300

    UPDATE

--- gui.py
+++ gui.py
@@ -1,3 +1,11 @@
+import subprocess
+import sys
+
+def install():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
+install()
+
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
 import subprocess
